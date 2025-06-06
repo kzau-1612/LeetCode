5688e6573dc25460c03b25642299ecb2b810a66e
kzau-1612 <kzau1612@gmail.com>
2025-06-07 00:00:56 +0700

    136

--- Array/1-TwoSum/script.js
+++ Array/1-TwoSum/script.js
@@ -1,15 +1,16 @@
 var nums = [2, 0, 11, 15, 7];
-var target = 9;
+// var nums = [3, 3];
+var target = 6;
 
 var twoSum = function (nums, target) {
   const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    // map.forEach((value, key) => console.log("Key: " + key + " Value: " + value + " i: " + i));
     const complement = target - nums[i];
     if (map.has(complement)) {
       return [map.get(complement), i];
     }
     map.set(nums[i], i);
+    console.log(map);
   }
   return null;
 };
@@ -19,4 +20,3 @@ console.log(twoSum(nums, target));
 
 var num = 121;
 // console.log(num.toString().length);
-console.log(1 % 10);
